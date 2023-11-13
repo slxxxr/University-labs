3e0abb14f2a946478f904752eee0eeee57f94e7a
Luka <98267810+slxxxr@users.noreply.github.com>
2023-11-13 20:50:36 +0300

    Create Treugolnik.h

---
+++ 4: Simplest classes/Treugolnik.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream> 
+#include <cmath>
+
+using namespace std;
+class Treugolnik {
+
+private:
+	double x1, y1, x2, y2, x3, y3;
+
+public:
+
+	Treugolnik();
+
+	Treugolnik(double x1, double y1, double x2, double y2, double x3, double y3);
+
+	Treugolnik(const Treugolnik& other);
+
+	void move(double x, double y);
+
+	double square();
+
+	bool operator < (Treugolnik& other);
+
+	bool operator > (Treugolnik& other);
+
+	Treugolnik& operator = (Treugolnik& other);
+
+	void operator *= (double n);
+
+	friend ostream& operator << (ostream& out, Treugolnik& T);
+
+	friend istream& operator >> (istream& in, Treugolnik& T);
+
+};
